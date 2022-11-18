6876d641abcb45b09760327836986e22a542bb82
God-FearingCoder01 <mhlopenkosikhona@gmail.com>
2022-11-18 12:08:17 +0200

    added the feature of pausing the welcome text

--- Chris' Calculator/info-display.c
+++ Chris' Calculator/info-display.c
@@ -6,13 +6,18 @@
 
 void display_welcome_info(void)
 {
-	printf("WELCOME TO CHRIS' SIMPLE CALCULATOR.\n");
-	printf("\tINSTRUCTIONS\n");
+	char c;
+	printf("WELCOME TO CHRIS' SIMPLE CALCULATOR.");
+	c = getchar();
+	printf("\n\tINSTRUCTIONS");
+	c = getchar();
 	printf("\tFirst enter an operator choice (and press Enter key).\n");
-	printf("\tThen enter operands next (on same line separated by a space).\n");
-	printf("\t\tOPERATOR CHOICE\n");
+	printf("\tThen enter operands next (on same line separated by a space).");
+	c = getchar();
+	printf("\n\t\tOPERATOR CHOICE\n");
 	printf("\t\tEnter 0 for + (addition)\n");
 	printf("\t\tEnter 1 for - (subtraction)\n");
 	printf("\t\tEnter 2 for * (multiplication)\n");
 	printf("\t\tEnter 3 for / (divition)\n");
+	c = getchar();
 }
