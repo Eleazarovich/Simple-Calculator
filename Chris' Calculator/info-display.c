260e6302fe2315c29133de9827476f0fcfe18a5a
God-FearingCoder01 <mhlopenkosikhona@gmail.com>
2022-11-18 12:43:35 +0200

    updated info-display file

--- Chris' Calculator/info-display.c
+++ Chris' Calculator/info-display.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * display_welcome_info - displays the startup information about the calculator
@@ -6,18 +7,17 @@
 
 void display_welcome_info(void)
 {
-	char c;
 	printf("WELCOME TO CHRIS' SIMPLE CALCULATOR.");
-	c = getchar();
+	pause_program();
 	printf("\n\tINSTRUCTIONS");
-	c = getchar();
+	pause_program();
 	printf("\tFirst enter an operator choice (and press Enter key).\n");
 	printf("\tThen enter operands next (on same line separated by a space).");
-	c = getchar();
+	pause_program();
 	printf("\n\t\tOPERATOR CHOICE\n");
 	printf("\t\tEnter 0 for + (addition)\n");
 	printf("\t\tEnter 1 for - (subtraction)\n");
 	printf("\t\tEnter 2 for * (multiplication)\n");
 	printf("\t\tEnter 3 for / (divition)\n");
-	c = getchar();
+	pause_program();
 }
